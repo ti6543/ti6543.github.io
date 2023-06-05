bda552dd8a67743434fcb0c43d9dab00f0f62a49
ti6543 <48279492+ti6543@users.noreply.github.com>
2023-06-05 13:58:26 +0100

    p

--- nav.js
+++ nav.js
@@ -1,54 +1,56 @@
-var navbar = ` <nav>
-<link rel="stylesheet" href="styles.css">
-<ul class="nav">
-  <li ><a href="index.html">Home</a></li>
+var navbar = ` 
 
+<nav class="navBar">
+    <div class="container">
+        <ul class="nav">
+            <li class="list1">Foil Knowledge</a>
+                <div class="nav1">
+                  <a href="whatisahyrdofoil.html" style="display:block">What is a hydrofoil?</a>
+                  <a href="frontwingsandstabs.html" style="display:block">Front wings and stabilisers</a>
+                  <a href="ridingafoil.html" style="display:block">How a foil is controlled</a>
+                  <a href="foilsetup.html" style="display:block">Foil setup</a>
+                  <a href="foilsetupadvanced.html" style="display:block">Foil setup (advanced)</a>
+                    <a href="foil_animation.html" style="display:block">Foil balance simulator</a>
+                    <a href="maneuverability.html" style="display:block">Maneuverability</a>
+                    <a href="drag.html" style="display:block">Drag</a>
+                    <a href="foilshapessizes.html" style="display:block">Foil shapes & sizes</a>
+                    <a href="choosingafoil.html" style="display:block">Choosing a foil</a>
+                  </div>
+               </li>
+               <li class="list1" >Wingfoil technique</a>
+                <div class="nav1">
+                    <a href="foiling.html" style="display:block">Getting Foiling</a>
+                    <a href="gybing.html" style="display:block">Gybing</a>
+                    <a href="tacking.html" style="display:block">Tacking</a>
+                </div>
+              </li>
+              
+                 <li class="list1">Weather
+                  <div class="nav1">
+                    <a href="SouthEastWeather.html" style="display:block">South East Weather</a>
+                </div>
+                </li>
+              
+                <li class="list1">Lessons
+                  <div class="nav1">
+                    
+                </div>
 
+                <li class="list1">Reviews
+                  <div class="nav1">
+                    
+                </div>
+              
+                <li class="list1">About
+                  <div class="nav1">
+                    
+                </div>
+                </li>
+        </ul>
+    </div>
+</nav>
 
-  <li class="list1">Foil Knowledge</a>
-    <div class="nav1">
-      <a href="whatisahyrdofoil.html" style="display:block">What is a hydrofoil?</a>
-      <a href="frontwingsandstabs.html" style="display:block">Front wings and stabilisers</a>
-      <a href="ridingafoil.html" style="display:block">How a foil is controlled</a>
-      <a href="foilsetup.html" style="display:block">Foil setup</a>
-      <a href="foilsetupadvanced.html" style="display:block">Foil setup (advanced)</a>
-        <a href="foil_animation.html" style="display:block">Foil balance simulator</a>
-        <a href="maneuverability.html" style="display:block">Maneuverability</a>
-        <a href="drag.html" style="display:block">Drag</a>
-        <a href="foilshapessizes.html" style="display:block">Foil shapes & sizes</a>
-        <a href="choosingafoil.html" style="display:block">Choosing a foil</a>
-      </div>
-   </li>
-
-   
-
-  <li class="list1" >Wingfoil technique</a>
-  <div class="nav1">
-      <a href="foiling.html" style="display:block">Getting Foiling</a>
-      <a href="gybing.html" style="display:block">Gybing</a>
-      <a href="tacking.html" style="display:block">Tacking</a>
-  </div>
-</li>
-
-   <li class="list1">Weather
-    <div class="nav1">
-      <a href="SouthEastWeather.html" style="display:block">South East Weather</a>
-  </div>
-  </li>
-
-  <li class="list1">Lessons
-    <div class="nav1">
-      
-  </div>
-
-  <li class="list1">About
-    <div class="nav1">
-      
-  </div>
-  </li>
-
-</ul>
-</nav>`;
+`;
 
 document.body.insertAdjacentHTML("beforeend", navbar);
 
