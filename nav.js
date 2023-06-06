1c04cbbc7b64f347fd39740f4e4cd4bd4fee98ef
ti6543 <48279492+ti6543@users.noreply.github.com>
2023-06-06 09:57:43 +0100

    p

--- nav.js
+++ nav.js
@@ -1,3 +1,5 @@
+function setnavbar() {
+
 var navbar = ` 
 <a class = "pic"  href="index.html" > <img src="figure_wing.png" height="60px" width = "60px"/></a>
 <nav class="navBar">
@@ -33,11 +35,9 @@ var navbar = `
                 </div>
                 </li>
               
-                
-              
-                <li class="list1">About
+                <li class="list1">Extras
                   <div class="nav1">
-                    
+                  <a href="about.html" style="display:block">About</a>
                 </div>
                 </li>
         </ul>
@@ -66,9 +66,64 @@ var navbar_knowledge = `
                     <a href="choosingafoil.html" style="display:block">Choosing a foil</a>
                   </div>
                </li>
+
+               </ul>
+               </div>
+           </nav>
+
+              <div class = "menu">
+               <img src="menu_icon.png" height="40px" width = "40px"/>
+               <div class="nav1">
+                <a href="foiling.html" style="display:block">Wingfoil technique</a>
+                <a href="SouthEastWeather.html" style="display:block">Weather</a>
+              </div>
+              </div>
+           
+              
+              
+                
+  
+`;
+
+var navbar_technique = ` 
+<a class = "pic"  href="index.html" > <img src="figure_wing.png" height="60px" width = "60px"/></a>
+<nav class="navBar">
+    <div class="container">
+        <ul class="nav">
+          
+
+        <li class="list1" >Wingfoil technique
+          <div class="nav1">
+            <a href="foiling.html" style="display:block">Getting Foiling</a>
+            <a href="gybing.html" style="display:block">Gybing</a>
+            <a href="tacking.html" style="display:block">Tacking</a>
+        </div>
+      </li>
                <li class="list1" ><img src="menu_icon.png" height="40px" width = "40px"/>
                 <div class="nav1">
-                    <a href="foiling.html" style="display:block">Techniques</a>
+                    <a href="whatisahyrdofoil.html" style="display:block">Foil Knowledge</a>
+                    <a href="SouthEastWeather.html" style="display:block">Weather</a>
+                </div>
+              </li>
+              
+                
+        </ul>
+    </div>
+</nav>
+`;
+
+var navbar_index = ` 
+<a class = "pic"  href="index.html" > <img src="figure_wing.png" height="60px" width = "60px"/></a>
+<nav class="navBar">
+    <div class="container">
+        <ul class="nav">
+          
+
+        
+               <li class="list1" ><img src="menu_icon.png" height="40px" width = "40px"/>
+                <div class="nav1">
+                    <a href="whatisahyrdofoil.html" style="display:block">Foil Knowledge</a>
+                    <a href="foiling.html" style="display:block">Wingfoil technique</a>
                     <a href="SouthEastWeather.html" style="display:block">Weather</a>
                 </div>
               </li>
@@ -88,17 +143,29 @@ let address = window.location.href.split("/").pop();
 const knowledge = ["whatisahyrdofoil.html", "frontwingsandstabs.html", "ridingafoil.html", "foilsetup.html", "foilsetupadvanced.html", "foil_animation.html", "maneuverability.html", "drag.html", "foilshapesandsizes.html", "choosingafoil.html"];
 let know = new Boolean(false);
 
+const technique = ["foiling.html", "gybing.html", "tacking.html"];
+let tech = new Boolean(false);
 
 for (let i = 0; i < knowledge.length; i++) {
   if (knowledge[i] == address) { know = true; }
+} 
+
+for (let i = 0; i < technique.length; i++) {
+  if (technique[i] == address) { tech = true; }
 } 
 
 
 
 
 if (know == true && width<700) {document.body.insertAdjacentHTML("beforeend", navbar_knowledge);}
+else if (tech == true && width<700) {document.body.insertAdjacentHTML("beforeend", navbar_technique);}
+else if (address == "index.html" && width<700) {document.body.insertAdjacentHTML("beforeend", navbar_index);}
 else {document.body.insertAdjacentHTML("beforeend", navbar);};
 
-console.log(width);
-console.log(address);
-//Hello
+}
+
+window.addEventListener("resize", setnavbar);
+
+setnavbar();
+
+
